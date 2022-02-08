1874f2aa3535d4b9b531762b4cb360dcec762ac0
Sofia <sofiaxrk@gmail.com>
2022-02-08 09:46:30 +0000

    primera parte detailsPlanets

---
+++ src/js/views/detailsPlanets.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router";
+import "../../styles/detailsPlanets.css"
+import { Context } from "../store/appContext";
+
+ export const DetailsPlanets = () => {
+
+const {store,actions} = useContext(Context)
+const params = useParams();
+
+    useEffect( ()=> {
+        actions.getPeopleById(params.peopleId)
+    },[]) 
+
+return(
+
+        <div className="Container-fluid">
+            <div className="row">
+                <div className="col-xl-12 wrappeople">
+                    <dic className="col-xl-7 peoplebucketimg">
+                        <img className="peoplephoto" src="https://pictures.abebooks.com/isbn/9781838544652-es.jpg"/>
+                    </dic>
+                    <div className="col-xl-5 peopledescription">
+                        <ul>
+                            <li>Nombre:</li>
+                            <li>Altura:</li>
+                            <li>Peso:</li>
+                            <li>Descripción:</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+
+};
