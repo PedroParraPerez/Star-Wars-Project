c14c3450a9b461bf71f2b7e221dce4a32b014361
PedroParraPerez <pedroparra06091994@gmail.com>
2022-02-14 15:12:09 +0000

    update

--- src/js/component/cardPeople.js
+++ src/js/component/cardPeople.js
@@ -2,14 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 
-const Card = (props) => {
+const CardPeople = (props) => {
   
 	return (
         <div className="card" >
         <img className="card-img-top" src="https://pictures.abebooks.com/isbn/9781838544652-es.jpg" alt="Card image cap"/>
         <div className="card-body">
               <h5 className="card-title">{props.title}</h5>
-              <p className="card-text">Aqui ira la descripcion</p>
               <Link to={"/detailspeople/".concat(props.uid)}>
 				<span className="btn btn-primary btn-lg" href="#" role="button">
 					Saber más
@@ -20,8 +19,8 @@ const Card = (props) => {
 	);
 };
 
-Card.propTypes = {
+CardPeople.propTypes = {
     title: PropTypes.string,
     uid: PropTypes.number,
 }
-export default Card;
+export default CardPeople;
