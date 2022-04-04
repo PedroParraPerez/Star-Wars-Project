821c6455fd73ddd7c2249122751e3dc3f5a9135c
PedroParraPerez <pedroparra06091994@gmail.com>
2022-04-04 08:46:13 +0000

    dropdown navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,28 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../../styles/navbar.css"
-import corazon from"../../img/corazon.png";
+import "../../styles/navbar.css";
+import corazon from "../../img/corazon.png";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light mb-3" id="fondoNavbar">
-			<Link to="/">
-					<img className="navbar-brand mb-0 h1" src="https://www.seekpng.com/png/full/10-105590_star-wars-logo-png-star-wars-png.png" alt="logo Star Wars Blog" id="logoNavbar"/>
-			</Link>
-			<div className="ml-auto">
-					<div className="dropdown">
-  						<button className="btn btn-dark dropdown-toggle" toggle="dropdown"><img className="favHearth" src={corazon} /></button>
-							<ul className="dropdown-menu">
-								<li>HTML</li>
-								<li>CSS</li>
-								<li>JavaScript</li>
-							</ul>
-					</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light mb-3" id="fondoNavbar">
+      <Link to="/">
+        <img
+          className="navbar-brand mb-0 h1"
+          src="https://www.seekpng.com/png/full/10-105590_star-wars-logo-png-star-wars-png.png"
+          alt="logo Star Wars Blog"
+          id="logoNavbar"
+        />
+      </Link>
+      <div className="ml-auto">
+        <div className="dropdown">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton1"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+           Favoritos
+          </button>
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <li className="favs_navbar">Action</li>
+            <li className="favs_navbar">Another action</li>
+            <li className="favs_navbar">Something else here</li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
-{/* <div className="listaFav">
+{
+  /* <div className="listaFav">
 						<ul className="listaFav">
 							<li className="listaFav"><h2>hola</h2></li>
 							<li className="listaFav"><h2>hola</h2></li>
@@ -30,4 +44,5 @@ export const Navbar = () => {
 							<li className="listaFav"><h2>hola</h2></li>
 							<li className="listaFav"><h2>hola</h2></li>
 						</ul>			
-					</div>	 */}
+					</div>	 */
+}
